9a8ece9c6027d27f27c1f3bc353f8eef82783f5c
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2023-04-19 20:48:02 -0300

    wip

--- evm.ts
+++ evm.ts
@@ -23,12 +23,90 @@ const MAX_UINT256 =
 const ZERO = BigInt(0);
 const ONE = BigInt(1);
 
-// TODO: COMPLETE
 enum OPCODES {
   STOP = 0x00,
-  PUSH0 = 0x5f,
+  ADD = 0x01,
+  MUL = 0x02,
+  SUB = 0x03,
+  DIV = 0x04,
+  SDIV = 0x05,
+  MOD = 0x06,
+  SMOD = 0x07,
+  ADDMOD = 0x08,
+  MULMOD = 0x09,
+  EXP = 0x0a,
+  SIGNEXTEND = 0x0b,
+  LT = 0x10,
+  GT = 0x11,
+  SLT = 0x12,
   SGT = 0x13,
+  EQ = 0x14,
+  ISZERO = 0x15,
+  AND = 0x16,
+  OR = 0x17,
+  XOR = 0x18,
   NOT = 0x19,
+  SHA3 = 0x20,
+  BYTE = 0x1a,
+  SHL = 0x1b,
+  SHR = 0x1c,
+  SAR = 0x1d,
+  ADDRESS = 0x30,
+  BALANCE = 0x31,
+  ORIGIN = 0x32,
+  CALLER = 0x33,
+  CALLVALUE = 0x34,
+  CALLDATALOAD = 0x35,
+  CALLDATASIZE = 0x36,
+  CALLDATACOPY = 0x37,
+  CODESIZE = 0x38,
+  CODECOPY = 0x39,
+  GASPRICE = 0x3a,
+  EXTCODESIZE = 0x3b,
+  EXTCODECOPY = 0x3c,
+  RETURNDATASIZE = 0x3d,
+  RETURNDATACOPY = 0x3e,
+  EXTCODEHASH = 0x3f,
+  COINBASE = 0x41,
+  TIMESTAMP = 0x42,
+  NUMBER = 0x43,
+  DIFFICULTY = 0x44,
+  GASLIMIT = 0x45,
+  CHAINID = 0x46,
+  SELFBALANCE = 0x47,
+  BASEFEE = 0x48,
+  POP = 0x50,
+  MLOAD = 0x51,
+  MSTORE = 0x52,
+  MSTORE8 = 0x53,
+  SLOAD = 0x54,
+  SSTORE = 0x55,
+  JUMP = 0x56,
+  JUMPI = 0x57,
+  PC = 0x58,
+  MSIZE = 0x59,
+  GAS = 0x5a,
+  JUMPDEST = 0x5b,
+  PUSH0 = 0x5f,
+  PUSH1 = 0x60,
+  PUSH32 = 0x7f,
+  DUP1 = 0x80,
+  DUP16 = 0x8f,
+  SWAP1 = 0x90,
+  SWAP16 = 0x9f,
+  LOG0 = 0xa0,
+  LOG1 = 0xa1,
+  LOG2 = 0xa2,
+  LOG3 = 0xa3,
+  LOG4 = 0xa4,
+  CREATE = 0xf0,
+  CALL = 0xf1,
+  RETURN = 0xf3,
+  DELEGATECALL = 0xf4,
+  STATICCALL = 0xfa,
+  REVERT = 0xfd,
+  SELFDESTRUCT = 0xff,
+  INVALID = 0xfe,
 }
 
 export default function evm(code: Uint8Array): Result {
@@ -40,18 +118,18 @@ export default function evm(code: Uint8Array): Result {
     pc++;
 
     // STOP
-    if (opcode === 0x00) {
+    if (opcode === OPCODES.STOP) {
       break;
     }
 
     // PUSH0
-    else if (opcode === 0x5f) {
+    else if (opcode === OPCODES.PUSH0) {
       stack.unshift(BigInt(0));
     }
 
     // PUSHX opcodes
-    else if (opcode > 0x5f && opcode <= 0x7f) {
-      let i = opcode - 0x5f;
+    else if (opcode > OPCODES.PUSH0 && opcode <= OPCODES.PUSH32) {
+      let i = opcode - OPCODES.PUSH0;
 
       let word = "";
       while (i > 0) {
@@ -66,12 +144,12 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // POP
-    else if (opcode === 0x50) {
+    else if (opcode === OPCODES.POP) {
       stack.shift();
     }
 
     // ADD
-    else if (opcode === 0x01) {
+    else if (opcode === OPCODES.ADD) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       let result = a + b;
@@ -83,12 +161,12 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // STOP
-    else if (opcode === 0x00) {
+    else if (opcode === OPCODES.STOP) {
       break;
     }
 
     // MUL
-    else if (opcode === 0x02) {
+    else if (opcode === OPCODES.MUL) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       let result = a * b;
@@ -100,7 +178,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // SUB
-    else if (opcode === 0x03) {
+    else if (opcode === OPCODES.SUB) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       let result = a - b;
@@ -112,7 +190,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // DIV
-    else if (opcode === 0x04) {
+    else if (opcode === OPCODES.DIV) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -125,7 +203,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // MOD
-    else if (opcode === 0x06) {
+    else if (opcode === OPCODES.MOD) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -138,7 +216,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // ADDMOD
-    else if (opcode === 0x08) {
+    else if (opcode === OPCODES.ADDMOD) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       const n: bigint = stack.shift()!;
@@ -152,7 +230,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // MULMOD
-    else if (opcode === 0x09) {
+    else if (opcode === OPCODES.MULMOD) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       const n: bigint = stack.shift()!;
@@ -166,7 +244,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // EXP
-    else if (opcode === 0x0a) {
+    else if (opcode === OPCODES.EXP) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
       const result = a ** b;
@@ -175,7 +253,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // SIGNEXTEND
-    else if (opcode === 0x0b) {
+    else if (opcode === OPCODES.SIGNEXTEND) {
       // https://ethereum.stackexchange.com/questions/63062/evm-signextend-opcode-explanation
       const size: bigint = stack.shift()!;
       const value: bigint = stack.shift()!;
@@ -193,33 +271,33 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // SDIV
-    else if (opcode === 0x05) {
-      const a: bigint = stack.shift()!;
-      const b: bigint = stack.shift()!;
+    else if (opcode === OPCODES.SDIV) {
+      const a: bigint = getBigIntFromTwoComplement(stack.shift()!);
+      const b: bigint = getBigIntFromTwoComplement(stack.shift()!);
 
-      let result = BigInt(0);
-      if (b != ZERO) {
-        result = a / b;
+      let result = ZERO;
+      if (a != ZERO && b != ZERO) {
+        result = getBigIntFromTwoComplement(a / b);
       }
 
       stack.unshift(result);
     }
 
     // SMOD
-    else if (opcode === 0x07) {
-      const a: bigint = stack.shift()!;
-      const b: bigint = stack.shift()!;
+    else if (opcode === OPCODES.SMOD) {
+      const a: bigint = getBigIntFromTwoComplement(stack.shift()!);
+      const b: bigint = getBigIntFromTwoComplement(stack.shift()!);
 
       let result = ZERO;
-      if (b != ZERO) {
-        result = a % b;
+      if (a != ZERO && b != ZERO) {
+        result = getBigIntFromTwoComplement(a % b);
       }
 
       stack.unshift(result);
     }
 
     // LT
-    else if (opcode === 0x10) {
+    else if (opcode === OPCODES.LT) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -228,8 +306,18 @@ export default function evm(code: Uint8Array): Result {
       stack.unshift(result);
     }
 
+    // GT
+    else if (opcode === OPCODES.GT) {
+      const a: bigint = stack.shift()!;
+      const b: bigint = stack.shift()!;
+
+      const result = a > b ? ONE : ZERO;
+
+      stack.unshift(result);
+    }
+
     // SLT
-    else if (opcode === 0x11) {
+    else if (opcode === OPCODES.SLT) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -239,7 +327,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // EQ
-    else if (opcode === 0x14) {
+    else if (opcode === OPCODES.EQ) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -249,7 +337,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // ISZERO
-    else if (opcode === 0x15) {
+    else if (opcode === OPCODES.ISZERO) {
       const a: bigint = stack.shift()!;
       const result = a == ZERO ? BigInt(1) : BigInt(0);
 
@@ -266,7 +354,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // AND
-    else if (opcode === 0x16) {
+    else if (opcode === OPCODES.AND) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -276,7 +364,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // OR
-    else if (opcode === 0x17) {
+    else if (opcode === OPCODES.OR) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -286,7 +374,7 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // XOR
-    else if (opcode === 0x18) {
+    else if (opcode === OPCODES.XOR) {
       const a: bigint = stack.shift()!;
       const b: bigint = stack.shift()!;
 
@@ -296,25 +384,39 @@ export default function evm(code: Uint8Array): Result {
     }
 
     // SHL
-    else if (opcode === 0x1b) {
+    else if (opcode === OPCODES.SHL) {
+      console.log("stack", stack);
+
       const shift: bigint = stack.shift()!;
       const value: bigint = stack.shift()!;
 
-      if (shift > 255) {
-        stack.unshift(ZERO);
-      } else {
-        let result = value << shift;
-        const hexResult = result.toString(16);
-        const hexResultLength = hexResult.length;
-        if (hexResultLength > 64) {
-          result = BigInt("0x" + hexResult.slice(hexResultLength - 64));
-        }
-        stack.unshift(result);
+      console.log(value.toString(16));
+
+      // if (shift > 255) {
+      //   stack.unshift(ZERO);
+      // } else {
+      let result = value << shift;
+      const hexResult = result.toString(16);
+      const hexResultLength = hexResult.length;
+      console.log(hexResultLength);
+      if (hexResultLength > 64) {
+        result = BigInt("0x" + hexResult.slice(hexResultLength - 64));
       }
+
+      // 111111110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+      // 1111111100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+      // ff00000000000000000000000000000000;
+      // 0xf000000000000000000000000000000000000000000000000000000000000000;
+
+      console.log(value.toString(16), value.toString(2));
+      console.log(result.toString(16), result.toString(2));
+
+      stack.unshift(result);
+      // }
     }
 
     // SLT
-    else if (opcode === 0x12) {
+    else if (opcode === OPCODES.SLT) {
       let a: bigint = stack.shift()!;
       let b: bigint = stack.shift()!;
 
@@ -337,6 +439,76 @@ export default function evm(code: Uint8Array): Result {
       const result = BigInt(a) > BigInt(b) ? ONE : ZERO;
 
       stack.unshift(result);
+    }
+
+    // SHR
+    else if (opcode === OPCODES.SHR) {
+    }
+
+    // SAR
+    else if (opcode === OPCODES.SAR) {
+    }
+
+    // BYTE
+    else if (opcode === OPCODES.BYTE) {
+    }
+
+    // DUPX opcodes
+    else if (opcode >= OPCODES.DUP1 && opcode <= OPCODES.DUP16) {
+      const i = opcode - OPCODES.DUP1;
+
+      const a: bigint = stack[i]!;
+
+      stack.unshift(a);
+    }
+
+    // SWAPX opcodes
+    else if (opcode >= OPCODES.SWAP1 && opcode <= OPCODES.SWAP16) {
+      const i = opcode - OPCODES.SWAP1 + 1;
+
+      const a: bigint = stack[0]!;
+      const b: bigint = stack[i]!;
+
+      stack[0] = b;
+      stack[i] = a;
+    }
+
+    // INVALID
+    else if (opcode === OPCODES.INVALID) {
+      return { success: false, stack };
+    }
+
+    // PC
+    else if (opcode === OPCODES.PC) {
+      stack.unshift(BigInt(pc));
+    }
+
+    // GAS
+    else if (opcode === OPCODES.GAS) {
+      stack.unshift(MAX_UINT256);
+    }
+
+    // JUMP
+    else if (opcode === OPCODES.JUMP) {
+      const counter = stack.shift()!;
+      const dest = code[counter]!;
+      if (dest !== OPCODES.JUMPDEST) {
+        return { success: false, stack };
+      }
+      pc = counter;
+    }
+
+    // JUMPI
+    else if (opcode === OPCODES.JUMPI) {
+      const counter = stack.shift()!;
+      const b = stack.shift()!;
+      if (b) {
+        const dest = code[counter]!;
+        if (dest !== OPCODES.JUMPDEST) {
+          return { success: false, stack };
+        }
+        pc = counter;
+      }
     }
 
     pc++;
